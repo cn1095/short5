826866362357db36b86b07dbe495ee90b655808e
cn1095 <1095053105@qq.com>
2025-12-26 21:12:30 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2819,6 +2819,35 @@ func initializeIPDatabases() {
 	// log.Println("IP数据库异步初始化完成")
 }
 
+func runAsDaemon() {
+	switch runtime.GOOS {
+	case "linux", "freebsd":
+		if os.Getppid() != 1 {
+			cmd := exec.Command(os.Args[0], os.Args[1:]...)
+			cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+			cmd.Stdout, cmd.Stderr, cmd.Stdin = nil, nil, nil
+			// 显式传递环境变量  
+            cmd.Env = os.Environ()
+			err := cmd.Start()
+			if err != nil {
+				log.Fatalf("后台运行失败: %v", err)
+			}
+			os.Exit(0)
+		}
+
+	case "windows":
+		cmd := exec.Command(os.Args[0], os.Args[1:]...)
+		err := cmd.Start()
+		if err != nil {
+			log.Fatalf("后台运行失败: %v", err)
+		}
+		os.Exit(0)
+
+	default:
+		log.Println("当前系统不支持后台模式")
+	}
+}
+
 func main() {
 
 	var (
@@ -3126,30 +3155,4 @@ func main() {
 	closeRedis()
 	ln.Close()
 	os.Exit(0)
-}
-func runAsDaemon() {
-	switch runtime.GOOS {
-	case "linux", "freebsd":
-		if os.Getppid() != 1 {
-			cmd := exec.Command(os.Args[0], os.Args[1:]...)
-			cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
-			cmd.Stdout, cmd.Stderr, cmd.Stdin = nil, nil, nil
-			err := cmd.Start()
-			if err != nil {
-				log.Fatalf("后台运行失败: %v", err)
-			}
-			os.Exit(0)
-		}
-
-	case "windows":
-		cmd := exec.Command(os.Args[0], os.Args[1:]...)
-		err := cmd.Start()
-		if err != nil {
-			log.Fatalf("后台运行失败: %v", err)
-		}
-		os.Exit(0)
-
-	default:
-		log.Println("当前系统不支持后台模式")
-	}
 }
